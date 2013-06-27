3b5b31cd936a863ea8839fe0a2ad1e38a53f2ee5
Matt Venn <matt@mattvenn.net>
2013-06-27 19:46:15 +0100

    fixed part1 pass script

--- part1/pass.py
+++ part1/pass.py
@@ -2,5 +2,5 @@ for file_num in range(1,101):
     file_name = str(file_num)
     fd = open(file_name,'w')
     for line in range(file_num):
-        fd.write("hello")
+        fd.write("hello\n")
     fd.close()
