e17a79b76bf51f5f2540920bdabef9f5c52317b2
Matt Venn <matt@mattvenn.net>
2013-06-28 21:02:30 +0100

    cp start_adventure to /usr/local/bin

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #sets up the adventure 
 #assumes the git repo has been checked out at /home/pi/raspi-adventure
+#run like ./raspi-adventure/setup.sh
+
+#make start_adventure accessible
+sudo cp raspi-adventure/start_adventure /usr/local/bin/
 
 #mv to .raspi-adventure, to hide the files
 mv raspi-adventure .raspi-adventure
 
 #requirements
-sudo apt-get -y install python-dev 
-sudo easy_install python-daemon
+sudo apt-get -y install python-dev  #need this for PIL
 sudo easy_install PIL
+sudo easy_install python-daemon
 sudo easy_install requests
 
 #start server
