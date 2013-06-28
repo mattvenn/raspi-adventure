5562667ef73c1d2bb0042c5fae7bb1cfc8452960
Matt Venn <matt@mattvenn.net>
2013-06-28 20:59:53 +0100

    attempts to install required packages

--- setup.sh
+++ setup.sh
@@ -5,5 +5,11 @@
 #mv to .raspi-adventure, to hide the files
 mv raspi-adventure .raspi-adventure
 
+#requirements
+sudo apt-get -y install python-dev 
+sudo easy_install python-daemon
+sudo easy_install PIL
+sudo easy_install requests
+
 #start server
 .raspi-adventure/checker.py
