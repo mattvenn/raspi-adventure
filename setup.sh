4c135d0010f388df076bd64dfed006c5c04c3658
matt venn <matt@mattvenn.net>
2013-06-28 20:54:25 +0100

    comments to bash

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-""" 
-sets up the adventure 
-assumes the git repo has been checked out at /home/pi/raspi-adventure
-"""
+#sets up the adventure 
+#assumes the git repo has been checked out at /home/pi/raspi-adventure
 
 #mv to .raspi-adventure, to hide the files
 mv raspi-adventure .raspi-adventure
